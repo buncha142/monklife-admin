04d1d5bd5fc4513bb67bec4c7cf39fc6eabd40ec
MiniBox <84363719+buncha142@users.noreply.github.com>
2023-10-26 20:28:08 +0700

    add-beeb

--- resources/views/livewire/crs/crs-lists.blade.php
+++ resources/views/livewire/crs/crs-lists.blade.php
@@ -26,6 +26,8 @@
                                     d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
                                     clip-rule="evenodd"></path>
                             </svg>
+                            <img src="image-components/Red_circle.gif" class="absolute w-96 opacity-75 {{ $bookcar->start_date->format('Y-m-d')==Carbon\Carbon::now()->format('Y-m-d') ? "" : "hidden" }}" />
+
                         </span>
                         <h3 class="flex items-center mb-1 text-xl font-semibold text-blue-900 ">{{ $bookcar->name }}
                         </h3>
